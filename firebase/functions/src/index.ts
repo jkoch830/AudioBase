df5944bde25b5bf0659fe4c0c018ebe3a44eac37
James Koch <jkoch830@gmail.com>
2020-07-30 19:20:01 -0400

    Firebase function uploads mp3

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,9 +1,37 @@
 import * as functions from 'firebase-functions';
+import * as ytdl from 'ytdl-core'
+import * as ffmpeg from 'fluent-ffmpeg'
+import * as ffmpeg_static from 'ffmpeg-static'
+import * as path from 'path'
+import * as os from 'os'
+import * as admin from 'firebase-admin'
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp()
+var bucket = admin.storage().bucket()
+
+// Makes an ffmpeg command return a promise.
+function promisifyCommand(command: ffmpeg.FfmpegCommand) {
+    return new Promise((resolve, reject) => {
+        command.on('end', resolve).on('error', reject).run();
+    });
+}
+
+/**
+ * Takes a URL from a youtube video, downloads the mp4 file, converts it to an
+ * mp3 file, then uploads it to Firebase Storage
+ */
+export const urlToMP3 = functions.https.onRequest(async (request, response) => {
+    const url = <string> request.query.url;
+    const name = <string> request.query.name
+    const tempFilePath = path.join(os.tmpdir(), name)
+    let proc = ffmpeg(ytdl(url))
+    .fromFormat('mp4')
+    .toFormat('mp3')
+    .setFfmpegPath(ffmpeg_static)            
+    .output(tempFilePath)
+    await promisifyCommand(proc)
+    const meta = {contentType: 'audio/mpeg'}
+    const uploadResponse = await bucket.upload(tempFilePath, {metadata: meta})
+    console.log(uploadResponse.values)
+    response.status(200).end()
+})
